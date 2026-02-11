bf971e0ec6f96b046d0c62c6925cfc93a4f8736d
Rodolfo Barcelli Jo <rodolfo.barcelli@gmail.com>
2026-02-11 15:09:34 +0800

    Update: colors changed

--- lua/rtheme/init.lua
+++ lua/rtheme/init.lua
@@ -5,24 +5,24 @@ function M.setup()
     vim.cmd('syntax reset')
 
     local colors = {
-        fg          = '#bfc5d2',
+        fg          = '#a0b8d1',
         bg          = '#000000',
-        black       = '#1a1b1d',
-        lblack      = '#242527',
-        red         = '#ff3551',
-        lred        = '#ff5f7c',
-        green       = '#4be451',
-        lgreen      = '#82ed7b',
-        yellow      = '#ff9a46',
-        lyellow     = '#ffd155',
-        blue        = '#77a8ff',
-        lblue       = '#81d8ff',
-        purple      = '#b582ff',
-        lpurple     = '#da7fff',
-        cyan        = '#33fad5',
-        lcyan       = '#99ffde',
-        white       = '#4c4e53',
-        lwhite      = '#b3b7c1',
+        black       = '#171b25',
+        lblack      = '#2c2e3e',
+        red         = '#d64f61',
+        lred        = '#f87591',
+        green       = '#54c266',
+        lgreen      = '#87dc79',
+        yellow      = '#ef963f',
+        lyellow     = '#fbda73',
+        blue        = '#667cf1',
+        lblue       = '#8fa8f8',
+        purple      = '#a086f0',
+        lpurple     = '#c18de8',
+        cyan        = '#44b079',
+        lcyan       = '#48cb9f',
+        white       = '#425059',
+        lwhite      = '#66767d',
     }
 
     local highlights = {
@@ -36,7 +36,7 @@ function M.setup()
         Constant        = { fg = colors.lcyan },
         Operator        = { fg = colors.fg },
         Delimiter       = { fg = colors.fg },
-        Type            = { fg = colors.fg },
+        Type            = { fg = colors.lpurple },
         Special         = { fg = colors.fg },
         Variable        = { fg = colors.fg },
         CursorLine      = { bg = colors.black },
@@ -51,16 +51,17 @@ function M.setup()
         -- Treesitter support
         ["@variable"]                   = { fg = colors.fg },
         ["@variable.parameter"]         = { fg = colors.yellow },
+        ["@type.builtin"]               = { fg = colors.lpurple },
         -- ["@type"]                       = { fg = colors.cyan },
         -- ["@type.builtin"]               = { fg = colors.cyan },
 
         -- Treesitter Markdown support
         ["@markup.heading.1.markdown"]  = { fg = colors.blue, bold = true, },
-        ["@markup.heading.2.markdown"]  = { fg = colors.orange, bold = true, },
-        ["@markup.heading.3.markdown"]  = { fg = colors.purple, bold = true, },
-        ["@markup.heading.4.markdown"]  = { fg = colors.red, bold = true, },
-        ["@markup.heading.5.markdown"]  = { fg = colors.cyan, bold = true, },
-        ["@markup.heading.6.markdown"]  = { fg = colors.green, bold = true, },
+        ["@markup.heading.2.markdown"]  = { fg = colors.purple, bold = true, },
+        ["@markup.heading.3.markdown"]  = { fg = colors.yellow, bold = true, },
+        ["@markup.heading.4.markdown"]  = { fg = colors.cyan, bold = true, },
+        ["@markup.heading.5.markdown"]  = { fg = colors.green, bold = true, },
+        ["@markup.heading.6.markdown"]  = { fg = colors.red, bold = true, },
 
         -- Todo support
         -- NOTE: THING
@@ -71,9 +72,9 @@ function M.setup()
         -- PERF: THING
         -- TEST: THING
 
-        TodoBgNOTE      = { bg = colors.lblue, fg = colors.bg, bold = true, italic = true },
-        TodoFgNOTE      = { fg = colors.lblue, italic = true },
-        TodoSignNOTE    = { fg = colors.lblue, italic = true },
+        TodoBgNOTE      = { bg = colors.blue, fg = colors.bg, bold = true, italic = true },
+        TodoFgNOTE      = { fg = colors.blue, italic = true },
+        TodoSignNOTE    = { fg = colors.blue, italic = true },
         TodoBgTODO      = { bg = colors.lcyan, fg = colors.bg, bold = true, italic = true },
         TodoFgTODO      = { fg = colors.lcyan, italic = true },
         TodoSignTODO    = { fg = colors.lcyan, italic = true },
