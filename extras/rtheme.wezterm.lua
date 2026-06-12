2eece58cacc16cce29e2cacc8605d8b1640f19e7
Rodolfo Barcelli Jo <rodolfo.barcelli@gmail.com>
2026-06-12 18:10:01 +0800

    Update settings in windows terminal theme, alacritty them and wezterm theme

---
+++ extras/rtheme.wezterm.lua
@@ -0,0 +1,62 @@
+return {
+  foreground = "#eeebff",
+  background = "#110f15",
+  cursor_bg = "#eeebff",
+  cursor_fg = "#110f15",
+  cursor_border = "#eeebff",
+  selection_fg = "#eeebff",
+  selection_bg = "#342d42",
+  scrollbar_thumb = "#342d42",
+  split = "#342d42",
+
+  ansi = {
+    "#241e2e",
+    "#ff4f7f",
+    "#a6ff65",
+    "#ffa770",
+    "#6daeff",
+    "#b483ff",
+    "#36ffc3",
+    "#706897",
+  },
+
+  brights = {
+    "#342d42",
+    "#ff79a0",
+    "#b9ff91",
+    "#ffd986",
+    "#76ddff",
+    "#fb97ff",
+    "#6fffe7",
+    "#a7a5cb",
+  },
+
+  indexed = {
+    [16] = "#ffa770",
+    [17] = "#ff4f7f",
+  },
+
+  tab_bar = {
+    background = "#110f15",
+    active_tab = {
+      bg_color = "#6daeff",
+      fg_color = "#110f15",
+    },
+    inactive_tab = {
+      bg_color = "#342d42",
+      fg_color = "#706897",
+    },
+    inactive_tab_hover = {
+      bg_color = "#342d42",
+      fg_color = "#eeebff",
+    },
+    new_tab = {
+      bg_color = "#110f15",
+      fg_color = "#706897",
+    },
+    new_tab_hover = {
+      bg_color = "#342d42",
+      fg_color = "#eeebff",
+    },
+  },
+}
